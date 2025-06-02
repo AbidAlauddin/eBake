8bf477dabd25046e4b74469a705dfd11eab355ab
ab1dal <96287687+ab1dal@users.noreply.github.com>
2025-06-03 04:48:54 +0700

    key

--- server/config/admin.js
+++ server/config/admin.js
@@ -6,4 +6,7 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
   },
+  auth: {
+    secret: env('AUTH_SECRET', crypto.randomBytes(16).toString('base64')),
+  },
 });
